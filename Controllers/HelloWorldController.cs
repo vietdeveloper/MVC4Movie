e9899e473821617b1ecd8e810d20095c932f2008
Viet <vietdeveloper@live.com>
2013-01-26 16:14:48 -0800

    Third commit
    Added a first view

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -8,9 +8,9 @@ namespace Mvc4Movie.Controllers
         //
         // GET: /HelloWorld/
 
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            return View();
         }
 
         //
