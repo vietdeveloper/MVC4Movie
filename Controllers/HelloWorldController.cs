acfc546861d6046d1692e03fcd80128cd19aff20
Viet <vietdeveloper@live.com>
2013-01-26 16:03:08 -0800

    Initial commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using System.Web;
+
+namespace Mvc4Movie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        //
+        // GET: /HelloWord/Welcome/
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello "+name+", NumTimes is: "+numTimes);
+        }
+    }
+}
